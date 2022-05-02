a58491688e3e705201b14ae4eaaf36f121445d0d
kyegordon3886 <kye.gordon.productions@gmail.com>
2022-05-02 13:52:47 -0500

    Added all event routes to event_routes.js
    All CRUD action files complete and ready for testing
    
    Co_Authored by: Alina Voey voey.alina@gmail.com

--- app/routes/event_routes.js
+++ app/routes/event_routes.js
@@ -28,7 +28,7 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // CREATE
-// POST /event
+// POST /events
 router.post('/events', requireToken, (req, res, next) => {
   // set owner of new example to be current user
   req.body.event.owner = req.user.id
@@ -43,5 +43,98 @@ router.post('/events', requireToken, (req, res, next) => {
     // can send an error message back to the client
     .catch(next)
 })
+
+// INDEX
+// GET /events
+router.get('/events', (req, res, next) => {
+  Event.find()
+    .then(events => {
+      // `events` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return events.map(event => event.toObject())
+    })
+    // respond with status 200 and JSON of the examples
+    .then(events => res.status(200).json({ events: events }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// SHOW
+// GET /events/5a7db6c74d55bc51bdf39793(ID)
+router.get('/events/:id', (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  Event.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is successful, respond with 200 and "event" JSON
+    .then(event => res.status(200).json({ event: event.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /events/5a7db6c74d55bc51bdf39793
+router.patch('/events/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.event.owner
+
+  Event.findById(req.params.id)
+    .then(handle404)
+    .then(event => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, event)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return event.updateOne(req.body.event)
+    })
+    // if that succeeded, return 204 and no JSON(will show 'no content')
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /events/5a7db6c74d55bc51bdf39793
+router.delete('/events/:id', requireToken, (req, res, next) => {
+  Event.findById(req.params.id)
+    .then(handle404)
+    .then(event => {
+      // throw an error if current user doesn't own `event`
+      requireOwnership(req, event)
+      // delete the event ONLY IF the above didn't throw error
+      event.deleteOne()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// UPDATE RSVP
+// PATCH /events/5a7db6c74d55bc51bdf39793(ID)
+// router.patch('/events/:eventId', requireToken, removeBlanks, (req, res, next) => {
+//   // if the client attempts to change the `owner` property by including a new
+//   // owner, prevent that by deleting that key/value pair
+//   delete req.body.event.owner
+
+//   Example.findById(req.params.id)
+//     .then(handle404)
+//     .then(example => {
+//       // pass the `req` object and the Mongoose record to `requireOwnership`
+//       // it will throw an error if the current user isn't the owner
+//       requireOwnership(req, example)
+
+//       // pass the result of Mongoose's `.update` to the next `.then`
+//       return example.updateOne(req.body.example)
+//     })
+//     // if that succeeded, return 204 and no JSON
+//     .then(() => res.sendStatus(204))
+//     // if an error occurs, pass it to the handler
+//     .catch(next)
+// })
+
+// will need request for RSVP, and will need to be required in from /models/RSVP
 
 module.exports = router
