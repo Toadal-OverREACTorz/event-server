ca1f8ca75a271947fd87415733859094448bc9a0
Alina <voey.alina@gmail.com>
2022-05-02 13:19:33 -0400

    Added event schema
    Co-Author: Jeff Rowland jrowla2@gmail.com
    Co-Author: Kyle Hough kye.gordon.productions@gmail.com

---
+++ app/models/events.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }, 
+    time: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }, 
+    // subdoc for rsvp schema
+    rsvp: [rsvpSchema],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
